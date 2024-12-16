7c36e5ef332e374c4b66e041efc2b8b02d66ae13
minij147 <jakepaul628@gmail.com>
2024-12-15 19:26:26 -0500

    added print information

--- assignment_1_submission_v2/prime-finder.go
+++ assignment_1_submission_v2/prime-finder.go
@@ -7,47 +7,64 @@ import (
 )
 
 const PRIME_MAX = 100000000 // 10^8
-// const PRIME_MAX = 10000
 const THREAD_CNT = 8
 const THREAD_WORKER_CNT = THREAD_CNT - 1
-const BATCH_SIZE = 5
+const THREAD_QUIT_FLAG = -1
 
 var isPrime = [PRIME_MAX + 1]bool{}
 var primeGlobal int64 = 0
 var jobWaiter sync.WaitGroup
 
+// sequential sieve of eratosthenes alogrithim
 func sieve() {
 	var p int64
 	for p = 2; p*p <= PRIME_MAX; p++ {
 		if isPrime[p] {
-			// work
 			primeGlobal = p
+
 			jobWaiter.Add(THREAD_WORKER_CNT)
 			jobWaiter.Wait()
 		}
 	}
 
-	primeGlobal = PRIME_MAX + 1 // set the quit flag
+	primeGlobal = THREAD_QUIT_FLAG // set the quit flag
 }
 
 // take some bound and calcualte for them
 func worker(id int64) {
-	for primeGlobal <= PRIME_MAX {
+	jobCnt := 0
+
+	for primeGlobal != THREAD_QUIT_FLAG {
 		primeLocal := primeGlobal
 
 		start := primeLocal * primeLocal
-		stagger := id * primeLocal
+		stagger := id * primeLocal // stagger for batch work
 
 		for i := start + stagger; i <= PRIME_MAX; i += THREAD_WORKER_CNT * primeLocal {
-			// fmt.Printf("worker: %v evluating: %v\n", id, i)
 			isPrime[i] = false
+			jobCnt++
 		}
 
 		jobWaiter.Done()
+
+		// spin until new prime is found
 		for primeLocal == primeGlobal {
 		}
 	}
-	fmt.Println("thread killed", id)
+	fmt.Printf("thread %v killed and completed %v jobs\n", id, jobCnt)
+}
+
+func printTop10Primes() {
+	list := [10]int{0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+	idx := 9
+	for i := PRIME_MAX; i > 0 && idx >= 0; i-- {
+		if isPrime[i] {
+			list[idx] = i
+			idx--
+		}
+	}
+
+	fmt.Println(list)
 }
 
 func main() {
@@ -62,12 +79,16 @@ func main() {
 	sieve()
 	totalTime := time.Since(start)
 
-	total := 0
+	primeTotal := 0
+	var primeSum int64 = 0
+
 	for i := 2; i <= PRIME_MAX; i++ {
 		if isPrime[i] {
-			// fmt.Printf("%v ", i)
-			total += 1
+			primeSum += int64(i)
+			primeTotal += 1
 		}
 	}
-	fmt.Println("Hello world!", total, totalTime)
+
+	fmt.Printf("Total Primes: %v | Prime Sum: %v | Total Time: %v | Top 10 Primes: ", primeTotal, primeSum, totalTime)
+	printTop10Primes()
 }
