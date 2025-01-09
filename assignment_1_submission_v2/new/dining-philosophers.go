7f3d3234b6547aebe830fd8a48e5bfdaa4539b88
MiniJ147 <jakepaul628@gmail.com>
2025-01-09 15:39:51 -0500

    finihed

--- assignment_1_submission_v2/new/dining-philosophers.go
+++ assignment_1_submission_v2/new/dining-philosophers.go
@@ -3,88 +3,73 @@ package main
 import (
 	"fmt"
 	"math/rand"
-	// "math/rand"
 	"sync/atomic"
 	"time"
 )
 
-const (
-	PHILO     = 4 
-	LOCK_SIZE = 2
-	SIM_DUR   = time.Second * 10
-
-	STATE_THINKING = "THINKING"
-	STATE_HUNGRY   = "HUNGRY"
-	STATE_EATING   = "EATING"
-)
+const PHILO = 5
+const SIM_DUR = 5 * time.Second
+const LOCK_LEN = 5
 
 var forks [PHILO]QueueLock
 
 type QueueLock struct {
-	head atomic.Int32
-	tail atomic.Int32
-	flag [LOCK_SIZE]bool
+	tail  atomic.Int32
+	head  atomic.Int32
+	flags [LOCK_LEN]bool
 }
 
-func (l *QueueLock) lock() {
-	slot := (l.tail.Add(1) - 1) % LOCK_SIZE
-	for !l.flag[slot] {
-
+func queueLockCreate() QueueLock {
+	return QueueLock{
+		flags: [LOCK_LEN]bool{true, false},
 	}
 }
 
-func (l *QueueLock) UnLock() {
-	slot := l.head.Load()
-	l.flag[slot%LOCK_SIZE] = false
-	l.flag[(slot+1)%LOCK_SIZE] = true
-	l.head.Add(1)
+func (l *QueueLock) Lock(id int) {
+	idx := (l.tail.Add(1) - 1) % LOCK_LEN
+	// fmt.Println(id, "got", idx)
+	for !l.flags[idx] {
+	}
 }
 
-func newLock() QueueLock {
-	return QueueLock{
-		flag: [2]bool{true, false},
+func (l *QueueLock) Unlock(id int) {
+	idx := l.head.Add(1)
+	// fmt.Println(id, "at", idx-1, "releasing")
+	l.flags[(idx-1)%LOCK_LEN] = false
+	l.flags[idx%LOCK_LEN] = true
+}
+
+func philosopher(id int) {
+	for {
+		idxLeft := (id + PHILO - 1) % PHILO
+		idxRight := id //(id + 1) % PHILO
+
+		fork1 := min(idxLeft, idxRight)
+		fork2 := max(idxLeft, idxRight)
+
+		fmt.Println(id, "is hungry taking", fork1, fork2)
+		forks[fork1].Lock(id)
+		forks[fork2].Lock(id)
+
+		fmt.Println(id, "is eating")
+		time.Sleep(time.Millisecond * time.Duration((500 + rand.Intn(2000))))
+
+		fmt.Println(id, "is thinking release", fork1, fork2)
+		forks[fork1].Unlock(id)
+		forks[fork2].Unlock(id)
+
+		time.Sleep(time.Millisecond * time.Duration((500 + rand.Intn(2000))))
 	}
 }
 
 func main() {
-	for i := 0; i < PHILO; i++ {
-		forks[i] = newLock()
+	for i := range PHILO {
+		forks[i] = queueLockCreate()
 	}
 
-	for i := 0; i < PHILO; i++ {
-		go func() {
-			state := STATE_THINKING
-			for {
-				time.Sleep(time.Second)
-				roll := rand.Intn(100)
-
-				if state == STATE_THINKING && roll >= 50 {
-					fmt.Printf("Philospher: %v is now hungry\n", i)
-					left := (i + PHILO - 1) % PHILO
-					right := (i + 1) % PHILO
-
-					forks[left].lock()
-					forks[right].lock()
-					fmt.Printf("Philospher: %v is eating\n", i)
-					time.Sleep(time.Millisecond * time.Duration((500 + rand.Intn(1500))))
-					fmt.Printf("Philospher: %v stopped eating\n", i)
-					forks[left].UnLock()
-					forks[right].UnLock()
-				}
-				// fmt.Printf("Philospher: %v is thinking\n", i)
-			}
-		}()
+	for i := range PHILO {
+		go philosopher(i)
 	}
-	// f := func(id int) {
-	// 	fmt.Println(id, "locking")
-	// 	forks[0].lock()
-	// 	fmt.Println(id, "in lock")
-	// 	time.Sleep(5 * time.Second)
-	// 	fmt.Println(id, "unlocking")
-	// 	forks[0].UnLock()
-	// }
-	//
-	// go f(1)
-	// go f(2)
+
 	time.Sleep(SIM_DUR)
 }
