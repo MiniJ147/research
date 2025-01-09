995b75c8f49b32f4b73edecd3dfa1aa69ac57bc2
minij147 <jakepaul628@gmail.com>
2025-01-08 19:41:13 -0500

    final changes to prime-finder

--- assignment_1_submission_v2/prime-finder.go
+++ assignment_1_submission_v2/prime-finder.go
@@ -3,92 +3,81 @@ package main
 import (
 	"fmt"
 	"sync"
+	"sync/atomic"
 	"time"
 )
 
 const PRIME_MAX = 100000000 // 10^8
 const THREAD_CNT = 8
-const THREAD_WORKER_CNT = THREAD_CNT - 1
-const THREAD_QUIT_FLAG = -1
+const BATCH_SIZE = 10
+const BATCH_SIZE_CNT = 50
 
-var isPrime = [PRIME_MAX + 1]bool{}
-var primeGlobal int64 = 0
-var jobWaiter sync.WaitGroup
+var nonPrime = [PRIME_MAX + 1]bool{true, true}
+var totalPrimes int64 = 0
+var primeSum int64 = 0
+var workDone int32 = 0
+var topPrimes = [10]int{-1, -1, -1, -1, -1, -1, -1, -1, -1, -1}
 
-// sequential sieve of eratosthenes alogrithim
-func sieve() {
-	var p int64
-	for p = 2; p*p <= PRIME_MAX; p++ {
-		if isPrime[p] {
-			primeGlobal = p
-
-			jobWaiter.Add(THREAD_WORKER_CNT)
-			jobWaiter.Wait()
-		}
-	}
-
-	primeGlobal = THREAD_QUIT_FLAG // set the quit flag
-}
-
-// take some bound and calcualte for them
-func worker(id int64) {
+func work(id int, wg *sync.WaitGroup) {
 	jobCnt := 0
+	var sum int64 = 0
 
-	for primeGlobal != THREAD_QUIT_FLAG {
-		primeLocal := primeGlobal
+	// run through your batches and caclualate primes
+	for i := (BATCH_SIZE * id); i <= PRIME_MAX; i += (BATCH_SIZE * THREAD_CNT) {
+		for j := 0; j <= BATCH_SIZE && j+i <= PRIME_MAX; j++ {
+			k := i + j
+			if !nonPrime[k] {
+				for z := k * k; z <= PRIME_MAX; z += k {
+					nonPrime[z] = true
+				}
+			}
 
-		start := primeLocal * primeLocal
-		stagger := id * primeLocal // stagger for batch work
-
-		for i := start + stagger; i <= PRIME_MAX; i += THREAD_WORKER_CNT * primeLocal {
-			isPrime[i] = false
 			jobCnt++
 		}
-
-		jobWaiter.Done()
-
-		// spin until new prime is found
-		for primeLocal == primeGlobal {
-		}
 	}
-	fmt.Printf("thread %v killed and completed %v jobs\n", id, jobCnt)
-}
 
-func printTop10Primes() {
-	list := [10]int{0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
-	idx := 9
-	for i := PRIME_MAX; i > 0 && idx >= 0; i-- {
-		if isPrime[i] {
-			list[idx] = i
-			idx--
+	// count primes
+
+	// if it is the last thread to be modifying the nonPrime Array should begin the grab
+	if atomic.AddInt32(&workDone, 1) == THREAD_CNT {
+		for i, j := PRIME_MAX, 9; i >= 0 && j >= 0; i-- {
+			if !nonPrime[i] {
+				topPrimes[j] = i
+				j--
+			}
 		}
 	}
 
-	fmt.Println(list)
+	// spin until all modifying threads are done
+	for workDone != THREAD_CNT {
+
+	}
+
+	//rerun over the batch and find the primes
+	var cnt int64 = 0
+	for i := (BATCH_SIZE_CNT * id); i <= PRIME_MAX; i += (BATCH_SIZE_CNT * THREAD_CNT) {
+		for j := 0; j <= BATCH_SIZE_CNT && j+i <= PRIME_MAX; j++ {
+			if !nonPrime[i+j] {
+				cnt++
+				sum += int64(i + j)
+			}
+		}
+	}
+
+	fmt.Printf("Thread %d did %d jobs\n", id, jobCnt)
+	atomic.AddInt64(&totalPrimes, cnt)
+	atomic.AddInt64(&primeSum, sum)
+	wg.Done()
 }
 
 func main() {
-	for i := 0; i <= PRIME_MAX; i++ {
-		isPrime[i] = true
-	}
-
+	var wg sync.WaitGroup
+	wg.Add(THREAD_CNT)
 	start := time.Now()
-	for i := 0; i < THREAD_WORKER_CNT; i++ {
-		go worker(int64(i))
+	for i := 0; i < THREAD_CNT; i++ {
+		go work(i, &wg)
 	}
-	sieve()
-	totalTime := time.Since(start)
-
-	primeTotal := 0
-	var primeSum int64 = 0
-
-	for i := 2; i <= PRIME_MAX; i++ {
-		if isPrime[i] {
-			primeSum += int64(i)
-			primeTotal += 1
-		}
-	}
-
-	fmt.Printf("Total Primes: %v | Prime Sum: %v | Total Time: %v | Top 10 Primes: ", primeTotal, primeSum, totalTime)
-	printTop10Primes()
+	wg.Wait()
+	end := time.Since(start)
+	fmt.Printf("Total Time: %v, total primes: %v, prime sum: %v, top primes %v\n", end, totalPrimes, primeSum, topPrimes)
 }
